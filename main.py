92b7c8f78d290700d89a0c4aa3dad8db3ccaaad8
Anton <cypku@mail.ru>
2025-06-26 16:20:23 +0300

    Selection

--- main.py
+++ main.py
@@ -379,3 +379,28 @@ for i in range (1, 10):
     for j in range(1, 10):
         print(f'{i} * {j} = {i*j}', end='\t')
     print()
+
+    # Подбор по росту
+    # 150 < height < 180
+    # Число кандидатов
+    # Число, кто прошёл по критерию
+    # Среди прошедших min и max
+    total = 0
+    total_success = 0
+    total_unsuccess = 0
+    min_val = float('inf')  # - бесконечность
+    max_val = float('-inf')  # + бесконечность
+
+    while (num := int(input('Введите рост: '))) != -1:
+        if 150 <= num <= 180:
+            total_success += 1
+            if min_val > num:
+                min_val = num
+            if num > max_val:
+                max_val = num
+        total += 1
+
+    print(f'Число кандидатов: {total}')
+    print(f'Число прошедших отбор: {total_success}')
+    print(f'Минимальный рост: {min_val}')
+    print(f'Максимальный рост: {max_val}')
