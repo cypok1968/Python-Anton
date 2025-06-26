4852c8197a1c5085435e02a2c82ab5e90a79eeac
Anton <cypku@mail.ru>
2025-06-26 11:56:22 +0300

    Loop continue

--- first project.py
+++ first project.py
@@ -1,24 +1,4 @@
 # Циклы (Loops):
-# while
-# while <условие>:
-#    команды
-# for
-counter = 0 # обнуляем счетчик
-# цикл из 5 итераций
-while counter < 5:
-    print(f'Итерация номер: {counter + 1}')
-    #counter = counter + 1 # инкремент
-    counter +=1 # инкремент (краткая запись)
-print(f'Итого в counter уже {counter}')
-
-
-counter = 5 # обнуляем счетчик
-# цикл из 5 итераций
-while counter > 0:
-    print(f'Итерация номер: {counter}')
-    counter = counter - 1 # инкремент
-    #counter -=1 # декремент (краткая запись)
-print(f'Обратный отсчёт: {counter}')
 
 
 
