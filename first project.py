ee5276f936b682cc6342f2f7062080282aa68161
Anton <cypku@mail.ru>
2025-06-26 14:59:54 +0300

    'for' loop begin

--- first project.py
+++ first project.py
@@ -1,9 +1,33 @@
-# ключевок слово in
+# цикл for
+# for <переменная> in ...:
+# команды
+#word = 'поток'
 
-word = 'поток'
+#for ch in word:
+#    print(ch)
 
-if 'ток' in word:
-    print('Есть')
+# итератор range (start, stop, step)
+
+#for i in range(2, 13, 2):
+#    print(i)
+
+# итератор range (0, stop, 1) - по умолчанию если не указаны 1 и 3 позиции
+
+#for i in range(13):
+#    print(i)
+
+#for i (или _) in range(10):
+#    print('Привет')
+
+for i in range(1, 101):
+    if i % 10 == 5:
+        if i == 15:
+            continue
+    print(i)
+
+for i in range(1, 101):
+    if i % 10 == 5 and i != 15:
+        print(i)
 
 
 
--- main.py
+++ main.py
@@ -304,3 +304,34 @@ while flag:
         flag = False
     case _:  # default
         print('Свернули налево')
+
+# цикл for
+# for <переменная> in ...:
+# команды
+#word = 'поток'
+
+#for ch in word:
+#    print(ch)
+
+# итератор range (start, stop, step)
+
+#for i in range(2, 13, 2):
+#    print(i)
+
+# итератор range (0, stop, 1) - по умолчанию если не указаны 1 и 3 позиции
+
+#for i in range(13):
+#    print(i)
+
+#for i (или _) in range(10):
+#    print('Привет')
+
+for i in range(1, 101):
+    if i % 10 == 5:
+        if i == 15:
+            continue
+    print(i)
+
+for i in range(1, 101):
+    if i % 10 == 5 and i != 15:
+        print(i)
