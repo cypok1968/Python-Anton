e596b4660f9e677915c6e8bf9a5a18784858278b
Anton <cypku@mail.ru>
2025-06-27 16:13:57 +0300

    Str Methods 1

--- first project.py
+++ first project.py
@@ -1,17 +1,18 @@
 # Строки (immutable, iterable)
-# Таблица символов Unicode
-# две удобные функции
-# ord(символ) - возвращает код символа в Unicode
-# chr(код в десятичной сист.) - возвращает символа Unicode-коду
+# Каждая буква повторяется столько раз,
+# какой её номер в строке (считаем с 1)
+
+word = 'статор'
+res = ''
+
+# 1 способ
+for i in range(len(word)):
+     print(word[i] * (i + 1), end='')
+# 2 способ
+for i in range(len(word) + 1):
+    print(word[i-1] * i, end='')
+
 
 #abc = 'абвгдеёжзийклмнопрстуфхцъыьэюя'
-phrase = 'Язык Phyton'
 
-print(phrase.lower()) # все маленькие
-print(phrase.upper()) # dct ,jkmibt
-print(phrase.capitalize()) # только первая буква заглавная
-print(phrase.title()) # все слова с заглавной
-print('Ура! ' * 3) # повторение строки
-print('Телевизор'.count('e')) # количество вхождений подстроки (число повторений символа)
-print('Python'.index('h')) # индекс символа, его номер в строке (012345...)
 
