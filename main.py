db19e58020320f9d403c20f95dee0cf0100d10b5
Anton <cypku@mail.ru>
2025-06-27 16:24:29 +0300

    Str Strip

--- main.py
+++ main.py
@@ -597,3 +597,17 @@ for i in s:
     res += chr(i)
 
 print(res)
+
+# Каждая буква повторяется столько раз,
+# какой её номер в строке (считаем с 1)
+
+word = 'статор'
+res = ''
+
+# 1 способ
+for i in range(len(word)):
+     print(word[i] * (i + 1), end='')
+# 2 способ
+for i in range(len(word) + 1):
+    print(word[i-1] * i, end='')
+
