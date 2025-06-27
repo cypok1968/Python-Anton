9929e60964df55992a79b0eea432079ada6ea8e5
cypok1968 <cypku1968@gmail.com>
2025-06-27 05:23:10 +0300

    Update first project.py

--- first project.py
+++ first project.py
@@ -1,24 +1,11 @@
-# Подбор по росту
-# 150 < height < 180
-# Число кандидатов
-# Число, кто прошёл по критерию
-# Среди прошедших min и max
-total = 0
-total_success = 0
-total_unsuccess = 0
-min_val = float('inf') # - бесконечность
-max_val = float('-inf') # + бесконечность
+# Д/З 26.06.25 нахождение фальшивой монеты методом взвешивания на рычажных весах
+weight A = input ('Введите вес первой монеты: ')
+weight В = input ('Введите вес второй монеты: ')
+weight С = input ('Введите вес третьей монеты: ')
 
-while (num := int(input('Введите рост: '))) != -1:
-    if 150 <= num <= 180:
-        total_success += 1
-        if min_val > num:
-            min_val = num
-        if num > max_val:
-            max_val = num
-    total += 1
-
-print(f'Число кандидатов: {total}')
-print(f'Число прошедших отбор: {total_success}')
-print(f'Минимальный рост: {min_val}')
-print(f'Максимальный рост: {max_val}')
+if weight A == weight В:
+    print('Фальшивая третья монета !')
+elif weight A > weight В:
+    print('Фальшивая вторая монета !')
+else:
+    print('Фальшивая первая монета !')
