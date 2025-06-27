77e3b49d589d81504070ea5cd5fc87a118034010
Anton <cypku@mail.ru>
2025-06-27 10:04:34 +0300

    Selection

--- first project.py
+++ first project.py
@@ -3,22 +3,34 @@
 # Число кандидатов
 # Число, кто прошёл по критерию
 # Среди прошедших min и max
-total = 0
-total_success = 0
-total_unsuccess = 0
-min_val = float('inf') # - бесконечность
-max_val = float('-inf') # + бесконечность
+# total = 0
+# total_success = 0
+# total_unsuccess = 0
+# min_val = float('inf') # - бесконечность
+# max_val = float('-inf') # + бесконечность
+#
+# while (num := int(input('Введите рост: '))) != -1:
+#     if 150 <= num <= 180:
+#         total_success += 1
+#         if min_val > num:
+#             min_val = num
+#         if num > max_val:
+#             max_val = num
+#     total += 1
+#
+# print(f'Число кандидатов: {total}')
+# print(f'Число прошедших отбор: {total_success}')
+# print(f'Минимальный рост: {min_val}')
+# print(f'Максимальный рост: {max_val}')
 
-while (num := int(input('Введите рост: '))) != -1:
-    if 150 <= num <= 180:
-        total_success += 1
-        if min_val > num:
-            min_val = num
-        if num > max_val:
-            max_val = num
-    total += 1
+# Д/З 26.06.25 "Hахождение фальшивой монеты методом взвешивания на рычажных весах"
+weighta = input ('Введите вес первой монеты: ')
+weightb = input ('Введите вес второй монеты: ')
+weightc = input ('Введите вес третьей монеты: ')
 
-print(f'Число кандидатов: {total}')
-print(f'Число прошедших отбор: {total_success}')
-print(f'Минимальный рост: {min_val}')
-print(f'Максимальный рост: {max_val}')
+if weighta == weightb:
+    print('Фальшивая третья монета !')
+elif weighta > weightb:
+    print('Фальшивая вторая монета !')
+else:
+    print('Фальшивая первая монета !')
