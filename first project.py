e637fbd938ca7a8a09f6a5cec269c9b0a64e0c82
Anton <cypku@mail.ru>
2025-06-30 10:44:19 +0300

    Stert and End of string

--- first project.py
+++ first project.py
@@ -1,7 +1,15 @@
 # Строки (immutable, iterable)
-
 # Начало и окончание строки
-#
+# startswith, endswith
+
+s = 'Смотреть'
+
+if s.lower().startswith('смо'):
+    print('Да')
+
+if s.endswith('еть'):
+    print('Да')
+
 
 
 
