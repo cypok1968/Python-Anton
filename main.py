d9f90dcdc090c3c83083db6a2a64b8279b803d81
Anton <cypku@mail.ru>
2025-06-30 14:44:28 +0300

    Stack

--- main.py
+++ main.py
@@ -775,3 +775,18 @@ print(res.title())
 # print(a)
 # print(b)
 # print(c)
+
+lst = [] # пустой список "окрошка"
+
+while (item := input('Ингредиент: ')) != '': # выбор ингредиентов окрошки
+    lst.append(item)
+
+temp = set(lst) # исключение повторов ингредиентов окрошки
+lst = list(temp)
+
+print(f'У нас есть {len(lst)} ингредиентов: ')
+
+lst.sort()
+
+for i in range(len(lst)):
+    print(f'\t{i+1}. {lst[i]}') # упорядочение вывода списка
