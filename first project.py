912a6ff23d5425099640807406927a8e1c74524e
Anton <cypku@mail.ru>
2025-06-30 15:32:30 +0300

    Unpack

--- first project.py
+++ first project.py
@@ -1,7 +1,7 @@
 # Кортеж (tuple) тот же список, но неизменяемый (отличие от строки)
-from main import cards
 
-#channels = ['red', 'green', 'blue']
+
+channels = ['red', 'green', 'blue']
 channels = [128, 200, 155]
 r, g, b = channels # распаковка unpack
 
