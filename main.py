011c5456bd30ed797c05ab3017362cc5f632cc5c
Anton <cypku@mail.ru>
2025-07-01 14:25:30 +0300

    Dictionary Methods(01.07.2025)

--- main.py
+++ main.py
@@ -939,7 +939,7 @@ for i, v in enumerate(fio): # нумерует элементы списка и 
     # list.sort()
     # for i in range(len(stop_list)):
     #     print(f'\t{i+1}. {stop_list[i]}')
-    # print(f'Исходный текст содержит список из {len(result)} слов и знаков: ')
+    # print(f'Искомый текст содержит список из {len(result)} слов и знаков: ')
     # result.sort()
     # for i in range(len(result)):
     #     print(f'\t{i+1}. {result[i]}')
@@ -952,6 +952,7 @@ for i, v in enumerate(fio): # нумерует элементы списка и 
     # res = sorted(temp)
     # for a, b in enumerate(res, 1):
     #     print(f'{a}. {b}')
+
     # commas = (',', '!', '.', '?', '-', ':')
     # text = input('Введите строку изначальной фразы: ').strip().lower()
     # stop_list = ['полна', 'неожиданностей', 'под', 'глазом', 'к', 'чаю'] # ввод запрещённых слов
@@ -1017,3 +1018,49 @@ text = 'Списочные выражения применяются для эф
 res = [a for a in text.split()[2::3]] # создание списка
 #res = set(a for a in text.split()[2::3]) # создание множества кортеж
 print(res)
+
+# Списочные выражения (list comprehension) 01.07.25
+# Вложенные списки - nested lists
+a = [1, 38.6, False, 'sfs', (1, 2)] # список может содержать данные любого типа, в том числе списки и выражения
+# создание вложенного списка
+N = 3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9], # висячая запятая, если вдруг нужно будет добавить ещё один вложенный массив
+]
+# замена всех элементов списков во вложенном списке на 1
+matrix = [[1] * N for _ in range(N)] # преобразование всех элементов вложенного списка размерности N
+# вместо i ставим _ потому, что индекс i нигде не используется
+print(matrix)
+
+# обход 2-мерного списка (матрицы)
+# for row in range(len(matrix)): # сначала выбираем ряд, начиная с 1-го
+#     for col in range(len(matrix[row])): # условие вывода каждого элемента очередного ряда, начиная с 1-го
+#
+# print(matrix[row][col])
+
+# возврат замененных элементов списков во вложенном списке
+count = 1
+for row in range(len(matrix)): # сначала выбираем ряд, начиная с 1-го
+    for col in range(len(matrix[row])): # условие вывода каждого элемента очередного ряда, начиная с 1-го
+        matrix[row][col] = count
+        count += 1
+print(matrix)
+
+matrix = []
+
+start = 1
+N = 4
+
+for i in range(N):
+    table = []
+    for j in range(start, start + N):
+        table.append(j)
+        matrix.append(table)
+        start += N
+print(matrix)
+
+N = 3
+matrix = [[i+j for j in range(N)]for i in range(1, 10, 3)]
+print(matrix)
