f985150234bd4a4a6b189cfd1bf26dd95ddf38cd
Anton <cypku@mail.ru>
2025-07-02 16:22:38 +0300

    Function as object(02/07/25)

--- first project.py
+++ first project.py
@@ -1,53 +1,25 @@
-#Функция с переменным числом аргументов, изменяется с помощью оператора ввода *args
-# эта функция позволяет ввести сначала именной аргумент first и переменное число арг. (за ним!)
+# Функция, как объект
+# Передается в другие функции: функции высшего порядка
 
+# печатник = print # печатник - объект, принявший ссылку на функцию print (не является копией)
+# печатник('Привет, мир')
 
-# def calc(*args: tuple, operator: str = '+') -> any:
-#     match operator:
-#         case '+':
-#             result = 0
-#             for i in args:
-#                 result += i
-#         case '*':
-#             result = 1
-#             for i in args:
-#                 result *= i
-#         case _: # аналог else - все остальные значения operator (по default)
-#             return 'Так нельзя'
-#     return result
-#
-#
-# print(calc(1,2,3, operator='*'))
+# Функция критерия отбора элементов списка
+# Критерий: длина слова
+def is_longer_six(word):
+    return len(word) > 6 # функция возвращает логическое значение T(F)
 
-def sandwich(type_of_meal, with_onion=False, with_tomato=False):
-    print('Булочка')
-    if with_onion:
-        print('Лук')
-    print(type_of_meal)
-    if with_tomato:
-        print('Помидоры')
-    print('Булочка')
+# Критерий - первая буква
+def is_first_letter_a(word):
+    return word[0] == 'а'
 
+fruits = ['арбуз', 'ананас', 'банан', 'ежевика', 'малина']
+res = list(filter(is_first_letter_a, fruits))
+print(res)
+words = ['В', 'этом', 'списке', 'останутся', 'слова',
+         'длина', 'которых', 'больше', 'шести']
+result = list(filter(is_longer_six, words))
+print(result)
 
-
-
-def print_any(*args, **kwarg):
-    for i in args:
-      print(i)
-    for k, v in kwarg.items():
-        print(k, '=', v) # создание словаря
-# **additional - Kwargs для резервирования дополнительной (неизвестной заранее) позиции для инфо
-def profile(name, surname, city, *children, **additional):
-    print(f'Имя: {name}')
-    print(f'Фамилия: {surname}')
-    print(f'Из города: {city}')
-    if len(children) > 0:
-        print('Дети:', ', '.join(children) )
-    print('Хобби:', ', '.join(additional['hobbies']))
-    # print(additional)
-
-profile('Дмитрий', 'Колесов', 'Волгоград',
-        'Мария', 'Пётр', 'Василий', hobbies=['Филателия', 'Шахматы'])
-
-# print_any('Дмитрий', 'Колесов', citi='Москва', age=27)
-# sandwich(type_of_meal='котлета', with_onion=True)
+for word in filter(is_longer_six, words):
+    print(word)
