3604dc25f702a1ab711a314b469044e065b5703b
Anton <cypku@mail.ru>
2025-07-02 12:42:56 +0300

    Operator is in practice (02/07/25)

--- first project.py
+++ first project.py
@@ -1,20 +1,32 @@
-# Оператор is: a is b -> когда a и b - один и тот же объект
-my_refregirator = ['колбаса', 'масло', 'сыр']
-# his_refregirator = ['колбаса', 'масло', 'сыр']
-#his_refregirator = my_refregirator # не создает новый объект, а только ссылку на тот же id-объект
-his_refregirator = my_refregirator.copy() # или [:] - создание копии с другим ID
-my_refregirator += ['мясо']
-print(his_refregirator)
-print(my_refregirator is his_refregirator)
-print(my_refregirator == his_refregirator)
-print(id(my_refregirator) == id(his_refregirator))
+def print_array(array: list, start: int = None):
+    if start is not None and start > len(array):
+        return
+    if start is None:
+        start = 0
+    for i in range(start, len(array)):
+            print(array[i])
 
-temp = None
-print(type(temp))
-print(temp is None) # print(temp == None) - запись сравнения не корректна, но работает
-temp = 1
-print(type(temp))
-print(temp == 1)
+
+a = [1, 2, 3]
+print_array(a, 1) # ,без start: n распечатывается весь список
+
+# Оператор is: a is b -> когда a и b - один и тот же объект
+# my_refregirator = ['колбаса', 'масло', 'сыр']
+# # his_refregirator = ['колбаса', 'масло', 'сыр']
+# #his_refregirator = my_refregirator # не создает новый объект, а только ссылку на тот же id-объект
+# his_refregirator = my_refregirator.copy() # или [:] - создание копии с другим ID
+# my_refregirator += ['мясо']
+# print(his_refregirator)
+# print(my_refregirator is his_refregirator)
+# print(my_refregirator == his_refregirator)
+# print(id(my_refregirator) == id(his_refregirator))
+#
+# temp = None
+# print(type(temp))
+# print(temp is None) # print(temp == None) - запись сравнения не корректна, но работает
+# temp = 1
+# print(type(temp))
+# print(temp == 1)
 
 # d = {'a': 1}
 # print(id(d))
