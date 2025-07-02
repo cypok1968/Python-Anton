8b130b1dfcba2a3ecad788eb43b77c84be82d735
Anton <cypku@mail.ru>
2025-07-02 12:31:13 +0300

    Is vs == (02/07/25)

--- first project.py
+++ first project.py
@@ -1,22 +1,31 @@
-# return vs yield - оба оператора возвращают значения переменных,
+# Оператор is: a is b -> когда a и b - один и тот же объект
+my_refregirator = ['колбаса', 'масло', 'сыр']
+# his_refregirator = ['колбаса', 'масло', 'сыр']
+#his_refregirator = my_refregirator # не создает новый объект, а только ссылку на тот же id-объект
+his_refregirator = my_refregirator.copy() # или [:] - создание копии с другим ID
+my_refregirator += ['мясо']
+print(his_refregirator)
+print(my_refregirator is his_refregirator)
+print(my_refregirator == his_refregirator)
+print(id(my_refregirator) == id(his_refregirator))
+
+temp = None
+print(type(temp))
+print(temp is None) # print(temp == None) - запись сравнения не корректна, но работает
+
+temp = 1
+print(type(temp))
+print(temp == 1)
+
+# d = {'a': 1}
+# print(id(d))
+# d['a'] += 1 # меняем изменяемый объект (словарь), при этом индекс не меняется
+# print(id(d))
+#
+# a = [0]
+# print(id(a))
+# a[0] += 1 # меняем изменяемый объект (список), при этом индекс не меняется
+# print(id(a))
 
 
-def print_goodbye(arg):
-    print('goodbye', end='')
-
-
-def print_cruel(arg):
-    print('cruel', end='')
-
-
-def print_world(arg):
-    print('world', end='')
-
-
-def main():
-    print_goodbye(1)
-    print_cruel(1)
-    print_world(1)
-
-main ()
 
--- main.py
+++ main.py
@@ -1337,3 +1337,25 @@ print(num_to_word(16))
 # array = tuple(generate_list())
 #
 # print(array)
+
+# return vs yield - оба оператора возвращают значения переменных,
+
+
+# def print_goodbye(arg):
+#     print('goodbye', end='')
+#
+#
+# def print_cruel(arg):
+#     print('cruel', end='')
+#
+#
+# def print_world(arg):
+#     print('world', end='')
+#
+#
+# def main():
+#     print_goodbye(1)
+#     print_cruel(1)
+#     print_world(1)
+#
+# main ()
