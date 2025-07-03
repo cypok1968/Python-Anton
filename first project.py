649cbc61c16d95f89f0fb4e0ebfab0d33a26e4f3
Anton <cypku@mail.ru>
2025-07-03 10:41:46 +0300

    Lambda Intro

--- first project.py
+++ first project.py
@@ -1,10 +1,39 @@
-# ДЗ 02.07.25
-# Превратить список nums в строку 123456789:
-# сначала с помощью функции map превратить в список строк, а затем использовать.join
+# Анонимные функции (однострочники, безымянные)
+# lambda-функции
+# lambda <аргументы>:<выражение>
 
-def square(num):
-    return num ** 2
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-squares = map(square, nums)
-print(list(squares))
+# def is_longer_six(word):
+#     return len(word) > 6
+#
+#
+# is_longer_six = lambda word: len(word) > 6
+#
+# def is_first_letter_(word):
+#     return world[0] == a
+#
+#
+#     return  word[0] ==
+
+fruits = ['арбуз', 'ананас', 'банан', 'малина', 'ежевика']
+
+res = list(filter(lambda x: x[0] == 'a', fruits))
+print(res)
+
+res = list(filter(lambda s: 'ан' in s, fruits))
+print(res)
+
+# в одну строку вывести список квадратов чисел от 3 до 15
+# [9,16,...]
+res = list(map(lambda y: y ** 2, range(3, 16)))
+print(res)
+res = [y ** 2 for y in range(3, 16)]
+print(res)
+
+words = ['В', 'этом', 'списке', 'останутся', 'слова',
+          'длина', 'которых', 'больше', 'шести']
+
+# result = list(filter(lambda word: len(word) > 6. words))
+
+long_words = [word for word in words if len(word) > 6]
+print(long_words)
