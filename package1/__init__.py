c4f82da219c3b87bf6ae35bfb97f668b75d9e3a8
cypok1968 <cypku1968@gmail.com>
2025-07-08 02:11:04 +0300

    Update __init__.py

--- package1/__init__.py
+++ package1/__init__.py
@@ -1,9 +1,10 @@
 # __init__.py показывает что работа идёт с пакетом package1
 from.module import greet # относительный упрощенный импорт из module
-from.utils import add # относительный упрощенный импорт из module
+from.module import factorial
+from.utils import add # относительный упрощенный импорт
 
 
 __version__ = '1.0.0' # версия пакета package1
 __doc__ = 'Этот пакет содержит...' # документация к пакету, комментарии
 __author__ = ('John')
-__all__ = ['greet', 'add'] # для импорта со *
+__all__ = ['greet', 'add', 'factorial'] # для импорта со *
