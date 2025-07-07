37e9c358d6730ecebf0b68ca8c773e2b5126268a
cypok1968 <cypku1968@gmail.com>
2025-07-08 02:13:24 +0300

    Update first project.py

--- first project.py
+++ first project.py
@@ -3,3 +3,9 @@
 # https://github.com/ipapMaster/Python_Web_2025.git - путь к учителю и его комитам на гитхаб.ком
 # https://disk.yandex.ru/d/9HNsXg77_qeidg - путь к методичкам
 # https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru - путь к шрифтам .ttf
+
+from package1 import module
+# from package1 import *
+
+print(int(module.factorial(5)))
+print(module.add(50, 80))
