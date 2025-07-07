eb891e5a64c0f0ca00f973a20696e4b923565c99
cypok1968 <cypku1968@gmail.com>
2025-07-08 00:17:31 +0300

    Update first project.py

--- first project.py
+++ first project.py
@@ -1,4 +1,5 @@
 # ДЗ 07/07/25
-# в Методичка.pdf
+# в Модули.pdf
 # https://github.com/ipapMaster/Python_Web_2025.git - путь к учителю и его комитам на гитхаб.ком
 # https://disk.yandex.ru/d/9HNsXg77_qeidg - путь к методичкам
+# https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru - путь к шрифтам .ttf
