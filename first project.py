7d5a1392f385259f885078c16f5c3710f00993ae
Anton <cypku@mail.ru>
2025-07-07 15:49:57 +0300

    Files multy string(07/07/25)

--- first project.py
+++ first project.py
@@ -3,9 +3,51 @@
 # name.txt (текстовые файлы, читаемые), бинарные (двоичные) в отличие текстовых не читаются (в смысле инфо)
 # t - текстовый файл (txt, html, xml)
 # b - бинарный файл (jpg, avi, mp3)
+# w - write (запись: файл создаётся или если он был, то всё что в нём было стирается)
+# a - append (добавление записи в конец файла, сам файл не стирается)
+# r - read (чтение: по умолчанию)
 
 
+# fo = open('info.txt', 'wt', encoding='utf-8') # запись в файл
 
+# fo.write('Хороший текст.')
+# print('\nА вот это будет уже с новой строки.', file=fo)
+# print('\nА вот ещё одна строка.', file=fo)
+
+fo = open('info.txt', 'rt', encoding='utf-8')
+
+# Построчное чтение № 1
+# while text := fo.readline():
+#     print(text.rstrip('\n'))
+
+# Построчное чтение № 2
+# lst = fo.readlines()
+# lst = list(map(lambda x: x.strip('\n'), lst))
+# print(lst)
+
+# Построчное чтение № 3
+text = fo.read()
+lst = text.splitlines()
+print(lst)
+
+fo.close()
+
+# text = fo.read(11) # в скобках указывается сколько начальных байт текста читать (для (3)='Это')
+# fo.read(6) # СЛЕДУЩЕЕ ЧТЕНИЕ НАЧИНАЕТСЯ С 12 позиции (со следующей)
+# text += fo.read(7)
+# print('Вот что было в файле', end=': ')
+# print(text)
+
+#fo.close() # лучше закрывать файлы в конце их вызова, даже только для чтения
+
+# fo = open('info.txt', 'wt', encoding='utf-8')
+# # print(fo.mode)
+# # print(fo.name)
+# # print(fo.encoding)
+#
+# count = fo.write('Этот текст будет в файле!')
+# print('В файл записано', count, 'байт!')
+# fo.close()
 
 # Документы по шаблону (из методичек https://disk.yandex.ru/d/9HNsXg77_qeidg
 # Внешние библиотеки
