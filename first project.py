364bbaeb682b2da0fd7f7da4b1d13f0f7aa28321
Anton <cypku@mail.ru>
2025-07-07 14:51:51 +0300

    Package of modules (07/07/25)

--- first project.py
+++ first project.py
@@ -1,3 +1,12 @@
+# Файлы это набор данных, сохраненных на носителе инфо определённой структуры, содержащем имя и расширени
+# расширение может ни о чём не говорить, если файл другой структуры
+# name.txt (текстовые файлы, читаемые), бинарные (двоичные) в отличие текстовых не читаются (в смысле инфо)
+# t - текстовый файл (txt, html, xml)
+# b - бинарный файл (jpg, avi, mp3)
+
+
+
+
 # Документы по шаблону (из методичек https://disk.yandex.ru/d/9HNsXg77_qeidg
 # Внешние библиотеки
 # Создаем и пишем свою библиотеку lib.py в проекте и подключаем её модули
@@ -8,18 +17,16 @@
 
 # from package1 import * # для __all__
 # from package1.module import greet
-from package1 import *
-
-
-
-print(greet('Мир'))
-print(add(3, 7))
-print('Автор')
-#print(package1.module._hidden_function()) # при попытке вывода результата скрытой функции _hidden_function
-                                          # выдается предупреждение об ограничении её использования
-
-
-import lib
+# from package1 import *
+#
+# print(greet('Мир'))
+# print(add(3, 7))
+# print('Автор')
+# #print(package1.module._hidden_function()) # при попытке вывода результата скрытой функции _hidden_function
+#                                           # выдается предупреждение об ограничении её использования
+#
+#
+# import lib
 #from lib import diff
 #from lib import summ
 # print(lib.diff(7, 3))
