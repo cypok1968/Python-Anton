0a77cdae7a4360dc14735c5de2fe2676e93dffff
cypok1968 <cypku1968@gmail.com>
2025-07-07 23:19:12 +0300

    Update first project.py

--- first project.py
+++ first project.py
@@ -1,4 +1,4 @@
 # ДЗ 07/07/25
 # в Методичка.pdf
-# https://github.com/ipapMaster/Python_Web_2025.git
-# https://disk.yandex.ru/d/9HNsXg77_qeidg
+# https://github.com/ipapMaster/Python_Web_2025.git - путь к учителю и его комитам на гитхаб.ком
+# https://disk.yandex.ru/d/9HNsXg77_qeidg - путь к методичкам
