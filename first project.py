402d52407f8c1ffaffe5cd4d2b38f9d0573e6c2b
Anton <cypku@mail.ru>
2025-07-08 13:02:10 +0300

    Exceptions: Task 2 (1) (08/07/25)

--- first project.py
+++ first project.py
@@ -8,20 +8,35 @@
 # finally:
 #     выполняется в любом случае
 
-#Задача 1.
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-try:
-    index = int(input('Введите индекс: '))
-    if -len(lst) < index < len(lst) -1:
-        raise ValueError('Индекс вне диапазона')
-    res = lst[index]
-    print(f'Число по индексу {index}: {lst[index]}')
-except ValueError as exp:
-    mess, val = exp.args
-    if mess[0].startswith('invalid literal'):
-        print(f'Вводить надо числа.')
-    else:
-        print(exp)
+# Задача 2 выявление исключений при делении двух чисел
+
+# 1-й способ с использованием цикла if
+while True:
+    a = input('Введите первое число: ')
+    b = input('Введите второе число: ')
+
+    if a.isdigit() and b.isdigit():
+        if int(b) == 0:
+            print('На ноль делить нельзя!')
+        else:
+            print(int(a) / int(b))
+            break
+    else:('Вводить надо только числа')
+
+# #Задача 1.
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# try:
+#     index = int(input('Введите индекс: '))
+#     if -len(lst) < index < len(lst) -1:
+#         raise ValueError('Индекс вне диапазона')
+#     res = lst[index]
+#     print(f'Число по индексу {index}: {lst[index]}')
+# except ValueError as exp:
+#     mess, val = exp.args
+#     if mess[0].startswith('invalid literal'):
+#         print(f'Вводить надо числа.')
+#     else:
+#         print(exp)
 
 
 # Утверждения (assertions) разработаны для поиска оптимального решения части программы
