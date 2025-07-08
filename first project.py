d754e40b7bfca496ab2981c586f0d341d3d8a6b2
Anton <cypku@mail.ru>
2025-07-08 10:28:59 +0300

    File Task (08/07/25)

--- first project.py
+++ first project.py
@@ -4,9 +4,38 @@
 # https://disk.yandex.ru/d/9HNsXg77_qeidg - путь к методичкам
 # https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru - путь к шрифтам .ttf
 
-from package1 import module
-# from package1 import *
+# res = [] # создаем пустой список
+#
+# with open('info.txt', 'rt') as f: # переписываем все элементы строк
+#     while temp := f.readline(): # читаем строку из файла
+#         res += temp.split(', ')
+#
+#
+# # res = list(map(lambda  x: x.rstip('\n'), res)) # отбрасываем ненужные символы
+# # res = set(res) # превращаем в множество
+#
+# res = set(list(map(lambda  x: x.rstip('\n'), res))) # упрощаем 2-е записи
+#
+# res = sorted((int(x) for x in res)) # формируем новый список переведенный в числа
+#
+# res = [int(x) for x in set(res)]
+#
+# print(res)
+
+res = [] # создаем пустой список
+
+with open('info.txt', 'rt') as f: # переписываем все элементы строк
+    while temp := f.readline().rstip('\n'): # читаем строки из файла
+        res += temp.split(', ')
 
 
-print(int(module.factorial(5)))
-print(module.add(50, 80))
+# res = list(map(lambda  x: x.rstip('\n'), res)) # отбрасываем ненужные символы
+# res = set(res) # превращаем в множество
+# res = set(list(map(lambda  x: x.rstip('\n'), res))) # упрощаем 2-е записи
+
+res = sorted((int(x) for x in res)) # формируем новый список переведенный в числа
+
+
+print(res)
+
+
