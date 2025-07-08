ac615728b417b18aa31d7663bf1958beb127e7bf
Anton <cypku@mail.ru>
2025-07-08 14:41:09 +0300

    Clear Dump VOC (08/07/25)

--- first project.py
+++ first project.py
@@ -7,24 +7,77 @@
 #     если исключения не было
 # finally:
 #     выполняется в любом случае
+# import pickle # процесс с применением т.н. "засолки"
+
+import pickle
+
+voc = {
+    'стол': 'table',
+    'стул': 'chair'
+}
+
+# функция для распечатки словаря
+def print_voc():
+    print('Сейчас словарь содержит: ')
+    for k, v in voc.items():
+        print(k, '-', v) # Alt + 0151
+
+# загружаем словарь из файла
+try:
+    with open('dict.dat', 'rb') as dump_in:
+        voc = pickle.load(dump_in)
+except FileNotFoundError:
+    with open('dict.dat', 'wb') as dump_out:
+        pickle.dump(voc, dump_out)
+    print('Создан минимальный словарь: ')
+    print_voc()
+
+while True:
+    temp = input('\nВведите слово для перевода или "#" для завершения: ')
+    word = temp.strip().lower()
+    if word == '#' or word == '№':
+        break
+    if word in voc.keys():
+        translate = voc[word]
+        print(f'Слово "{word}" переводится как {translate}.\n')
+    else:
+        print(f'Значение слова {word} отсутсвует в словаре.')
+        newkey = f'А как слово {word} переводится?'
+        newkey += 'Если ничего не вводите нажмите ENTER, \n'
+        newkey  += 'или введите его здесь: '
+        new_word = input(newkey)
+
+        if new_word != '' or len(new_word) > 2:
+            voc[word] = new_word
+            print(f'Слово {word} с переводом {new_word} внесено в словарь.')
+        else:
+            print('Ничего не введено или слишком короткое слово.')
+            continue
+
+# Необходимо сохранить новый словарь
+with open('dict.dat', 'wb') as dump_out:
+    pickle.dump(voc, dump_out)
+
+print('До новых встреч!!!')
+
 
 # Задача 2 выявление исключений при делении двух чисел
 
 # 2-й способ с использованием цикла try
 
-while True:
-    a = input('Введите первое число: ')
-    b = input('Введите второе число: ')
-    try:
-        result = int(a) / int (b)
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!!!')
-    except ValueError:
-        print('Нужно вводить числа...')
-        print(f'А введено: {a} и {b} :(')
-    else:
-        print(result)
-        break
+# while True:
+#     a = input('Введите первое число: ')
+#     b = input('Введите второе число: ')
+#     try:
+#         result = int(a) / int (b)
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя!!!')
+#     except ValueError:
+#         print('Нужно вводить числа...')
+#         print(f'А введено: {a} и {b} :(')
+#     else:
+#         print(result)
+#         break
 
 # 1-й способ с использованием цикла if
 # while True:
