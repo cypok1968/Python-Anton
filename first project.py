e25aec2691a15342837206205a2cb81e7cabd04d
Anton <cypku@mail.ru>
2025-07-08 10:46:57 +0300

    (08/07/25)

--- first project.py
+++ first project.py
@@ -4,6 +4,26 @@
 # https://disk.yandex.ru/d/9HNsXg77_qeidg - путь к методичкам
 # https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru - путь к шрифтам .ttf
 
+import pickle # процесс с применением т.н. "засолки"
+import pprint
+
+# d = {
+#     'стол': 'table',
+#     'стул': 'chair'
+# }
+#
+# # сериализация (последовательность элементов файла (словаря) помещают, преобразовывают в компактный файл, набор байт нечитаемый)
+# with open('dictfile.dat', 'wb') as p: # открываем бинарный файл
+#     pickle.dump(d, p) # d - что сериализуем, p - куда сериализуем
+
+# десериализация (является небезопасной, т.к. возможно проникновение вирусов, действующих на уровне прав ОС)
+with open('dictfile.dat', 'rb'):  # открываем файл
+    d = pickle.load(p)  # загружаем файл (читаем, как p)
+
+pprint.pprint(d, width=15)
+
+
+
 # res = [] # создаем пустой список
 #
 # with open('info.txt', 'rt') as f: # переписываем все элементы строк
@@ -22,19 +42,16 @@
 #
 # print(res)
 
-res = [] # создаем пустой список
-
-with open('info.txt', 'rt') as f: # переписываем все элементы строк
-    while temp := f.readline().rstip('\n'): # читаем строки из файла
-        res += temp.split(', ')
-
-# res = list(map(lambda  x: x.rstip('\n'), res)) # отбрасываем ненужные символы
-# res = set(res) # превращаем в множество
-# res = set(list(map(lambda  x: x.rstip('\n'), res))) # упрощаем 2-е записи
-
-res = sorted((int(x) for x in res)) # формируем новый список переведенный в числа
-
-
-print(res)
-
-
+# res = [] # создаем пустой список
+#
+# with open('info.txt', 'rt') as f: # переписываем все элементы строк
+#     while temp := f.readline().rstip('\n'): # читаем строки из файла
+#         res += temp.split(', ')
+#
+# # res = list(map(lambda  x: x.rstip('\n'), res)) # отбрасываем ненужные символы
+# # res = set(res) # превращаем в множество
+# # res = set(list(map(lambda  x: x.rstip('\n'), res))) # упрощаем 2-е записи
+#
+# res = sorted((int(x) for x in res)) # формируем новый список переведенный в числа
+#
+# print(res)
