91749d5cd3fabc7dfe17b862c19cc775e8f74635
cypok1968 <cypku1968@gmail.com>
2025-07-08 13:33:30 +0300

    Update module.py

--- package1/module.py
+++ package1/module.py
@@ -11,13 +11,6 @@ def factorial (n):
         return n * factorial(n-1)
 
 
-def factorial (n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-
 # Скрытая функция (имя с обязательным первым символом "подчёркивания")
 def _hidden_function():
     return 'Для внутреннего пользования'
