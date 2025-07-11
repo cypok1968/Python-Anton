c9e6b1c03e4252e4af2edbb23c6c7ac11311d0f1
Anton <cypku@mail.ru>
2025-07-11 12:15:33 +0300

    Net Intro and CLI common light interface (11/07/25)

--- first project.py
+++ first project.py
@@ -1,442 +1,70 @@
-# ДЗ 10/07/25 в Телеграм
+#          Web
+#          Протоколы
 
-# OOП Class
+# Протокол -
+# совокупность правил, регламентирующих функции передачи данных
+# между компонентами компьютерных сетей (правила передачи данных в комп.сетях)
 
-from abc import ABC, @abstractmethod
+# Transmission Control Protocol (TCP) - протокол управления передач, управляет потоком пакетов по маршруту
+# и контролирует сохранность передачи пакетов, если один из пакетов бракованный, то требует его переотправки
+# Internet Protocol (IP) - разбиение электронных данных на пакеты (IP-дейтаграммы), определяет куда они направляются
+# TCP/IP - стандартная запись двух протоколов (т.к. всегда работают в связке)
+# HTTP (Hyper Text Transfer Protocol) - протокол передачи гипертекста (т.е. документа формата *.HTML)
+# https://www. - протокол передачи данных для браузера
+# FTP - File Transfer Protocol протокол передачи файлов
+# SMTP - Simple Mail Transfer Protocol передача
+# Хост-система - компьютер являющийся частью сети, по которой другие компьютеры могут подключаться к нему, как клиенты
+# и обмениваться с ним информацией
+# 1. Обязательная - IP-адрес (дружественная компьютеру, неудобная пользователю)
+# Обязательная - IP-адрес: 195.34.32.11 (длина адреса 0-255, 1-я часть адрес сети (класс сети),
+# 2-я часть адрес компьютера этой сети)
 
-class Animal:
-    @abstractmethod
-    def make_sound(self):
-        pass
+# 2. Необязательная (удобная абоненту, не дружественная компу)
+# Необязательная - DNS (Domain Name System)
+# https://www.yandex.msk.ru - удобно для абонента задаётся IP-адрес маршрутизатору
+# (ресурс, город, страна в строке)
 
+# При создании сайта необходим надёжный сервер как Хост-комп с бесперебойным питанием,
+# постоянным охлаждением, большим количеством жестких дисков,
+# которому сразу присваивается DNS и соотв.IP-адрес
 
-class Dog(Animal):
-    def make_sound(self):
+# ASCII %20 (пробел в 16-ной системе), %2С (запятая)
+# URL - Uniform Resource Locator
+# в нём запрещены пробелы, знаки препинания из ASCII
+# https://www.yandex.ru - при создании доменных имён они проверяются на сайтах nick.ru, reg.ru
+# (где можно также купить доменные имена) пример создания своего доменного имени
+# quick.ru -> quickitaly.ru
+#http(s)://домен.зона/page1/?param1=value1&param2=value2 (метод Get, получения конкретной инф-ии,
+# после обращения к нужной странице задаем для конкретного места, форимруется длинный скрипт-путь)
 
+# Командная строка (простота удалённой работы, запускаем цепочки механизмов автоматизации)
+# dir - пролистываем содержимое каталога, просмотр файлов, дат создания, объема
+# ls - то же самое для Линукса
+# mk dir - создать директорию
+# ./images - из текущуго каталога идём в images
+# ../images - сначала на один уровень выше, потом в каталог
+# rm -r templates - заходит в дирректорию templates
+# и удаляет все файлы без права на их восстановление, а затем саму директорию
 
+# cp (источник) (пункт назначения) -копирование файла в другую директорию (copy)
+# mk dir - создать директорию
+# mv - перемещение файла в другую директорию с переименованием
 
-class Cat(Animal):
-    def make_sound(self):
+print('hello world')
 
+import sys
 
-class Cat(Animal):
-    def make_sound(self):
+print('Я', sys.argv[0], 'и мой аргумент', sys.argv[1]) # запускаем скрипт с какими-то доп. вх. аргументами (ключами)
 
+print(len(sys.argv))
 
+if len(sys.argv) >= 2: # задание ключей для формализации процедур создания функций
+                       # для их реализации через командной строки (например уже готовых шаблонов файлов для проекта)
+    match sys.arg[1]: # здесь тривиальная реализация обращения через ключи аргуметов
+        case 'p':
+            print('Привет')
+        case 'g':
+            print('Пока')
+        case _:
+            print('Не понял')
 
-class (Zoo):
-    def add_animal
-
-
-
-
-dog = Dog()
-cat = Cat()
-
-zoo = Zoo()
-
-zoo.add_animal(dog)
-
-
-
-
-"""
-Задача: создать классы для моделирования клиента банка и их счетов.
-Условия:
-Класс BankAccount с атрибутами: _owner_name, balance.
-Методы: deposit(amount), withdraw(amount), get_balance().
-"""
-# class  BankAccount:
-#     def __init__(self, owner, balance=0):
-#         self._owner = owner
-#         self._balance = balance
-#
-#     def get_balance(self):
-#         return self._balance
-#
-#     def deposit(self, amount):
-#         if amount > 0:
-#             self._balance += amount
-#             print(f'Депозит пополнен на сумму: {amount}.')
-#         else:
-#             print(f'Нельзя вносить отрицательную сумму на депозит.')
-#
-#
-#     def withdraw(self, amount):
-#         if 0 < amount <= self._balance:
-#             self._balance -= amount
-#             print(f'с депозита снята сумма: {amount}.')
-#         else:
-#             print(f'Не хватает средств. Овердрафт не доступен')
-#
-#
-# client1 = BankAccount('John')
-# client1.deposit(1000)
-# client1.withdraw(600)
-# print('Остаток: ', client1.get_balance())
-
-
-
-# ООП (inheritance)
-# класс, от которого наследуем: базовый, родительский, суперкласс
-# класс-наследник, который наследуется: дочерний,
-
-# # Фигуры
-# from math import pi
-# from abc import ABC, abstractmethod
-# #class Shape(object): # высший класс по иерархии для всех классов object,
-#                      # в скобках его можно не указывать (по умолчанию), наследуются все его методы
-# class Shape(ABC):
-#     def info(self):
-#         print(f'Класс: {self.__class__.__name__}')
-#
-#     @abstractmethod
-#     def area(self):
-#         pass
-#
-#     @abstractmethod
-#     def perimetr(self):
-#         pass
-#
-#
-# class Circle(Shape):
-#     def __init__(self, radius):
-#         self.radius = radius
-#         self.name = 'круг' # при наследовании базовом суперклассе Shape класс определён и
-#                         # нет необходимости в выводе имени отдельного класса
-#
-#     def perimetr(self):
-#         return 2 * pi * self.radius
-#
-#     def area(self):
-#         return pi * self.radius ** 2
-#
-#     def get_name(self):
-#         return self.name
-#
-# class Rectangle(Shape): # базовый суперкласс, от которого наследуем в Square
-#     def __init__(self, width, height):
-#         self.width = width
-#         self.height = height
-#         self.name = 'прямоугольник' # при наследовании базовом суперклассе Shape класс определён и
-#                                     # нет необходимости в выводе имени отдельного класса
-#
-#     def perimetr(self):
-#         return 2 * (self.width + self.height)
-#
-#     def area(self):
-#         return self.width * self.height
-#
-#     def get_name(self):
-#         return self.name
-#
-#
-# class Square(Rectangle, Shape): # ссылка на наследование базового метода Rectangle
-#     def __init__(self, side):
-#         super().__init__(side, side)  # задаем для квадрата одинаковые стороны
-#                                      # для конструктора базового класса
-#         # self.side = side # строка становится не нужна при наследовании
-#         self.name = 'квадрат' # при наследовании базовом суперклассе Shape класс определён и
-#                               # нет необходимости в выводе имени отдельного класса
-#
-# class Triangle(Square):  # ссылка на наследование базового метода Rectangle
-#     def __init__(self, side):
-#         super().__init__(side)
-#         self.side = side
-#         self.name = 'треугольник'
-#
-#     def area(self):
-#         return (self.side ** 2 * 3 * 0.5) / 4
-#
-#         def perimetr(self):
-#             return self.side * 3
-#
-#
-#     # def perimetr(self): # при наследовании квадратом определения прямоугольника
-#     #                     # эти процедуры, только для квадрата становятся не нужны (не влияют на рез-т)
-#     #     return 4 * self.side
-#     #
-#     # def area(self):
-#     #     return self.side ** 2
-#
-#     # def get_name(self):
-#     #     return self.name
-#
-#
-# c = Circle(5)
-# print(c.area())
-# print(c.perimetr())
-# print(c.get_name())
-# c.info()
-#
-# s = Square(5)
-# print(s.area())
-# print(s.perimetr())
-# print(s.get_name())
-# s.info()
-#
-# r = Rectangle(5, 4)
-# print(r.area())
-# print(r.perimetr())
-# print(r.get_name())
-# r.info()
-#
-# t = Triangle(5)
-# print(t.area())
-# print(t.perimetr())
-# print(t.get_name())
-# t.info()
-
-# ООП (magic methods) - специальные методы
-# для нужд отладки и визуализации
-
-# __call__ - экземпляр класса становится вызываемым (как функция)
-# y = ax^2 + bx + c
-# class SquareFunction:
-#     def __init__(self, a, b, c):
-#         self.a = a
-#         self.b = b
-#         self.c = c
-#
-#     def __call__(self, x):
-#         return self.a * x ** 2 + self.b * x + self.c
-#
-#
-# s = SquareFunction(1, 2, 3) # вызов экземпляра s (со свойствами функции)
-# print(s(2))
-
-# class MyTime:
-#     def __init__(self, minutes, seconds):
-#         if 0 <= minutes < 60:
-#             self.minutes = minutes
-#         if 0 <= seconds < 60:
-#             self.seconds = seconds
-#
-#     def __add__(self, other):
-#         m = self.minutes + other.minutes
-#         s = self.seconds + other.seconds
-#         m += s//60
-#         s = s % 60 # сумма не должна выходить за ограничение 60
-#         m = m % 60
-#         print(m, s)
-#         return MyTime(m, s)
-#
-#         #return f'Сумма минут: {self.minutes + other.minutes}, сумма секунд: {self.seconds + other.seconds}'
-#
-#     def __str__(self):
-#         return f'<Time {self.minutes:02}:{self.seconds:02}>'
-#
-# t1 = MyTime(13, 5)
-# t2 = MyTime(53, 0)
-# print(t1 + t2)
-# t = MyTime(13, 15)
-# print(t)
-
-
-
-# from math import hypot
-#
-# class Point:
-#     def __init__(self, x=0, y=0):
-#         self.x = x
-#         self.y = y
-#
-#     def __str__(self):
-#         return f'Point: ({self.x}, {self.y})>'
-#
-#     def __repr__(self):
-#         return f'Points: ({self.x}, {self.y})'
-#
-#     def __sub__(self, other):
-#         #return Point(self.x - other.x, self.y - other.y) # вывод расстояний между 2-мя (.)
-#                                                           # на коорд.плоскости с учетом знака
-#         return Point(abs(self.x - other.x), abs(self.y - other.y)) # ...абсолютное значение
-#
-#     # Расчёт расстояния между точками на координатной плоскости
-#     def __add__(self, other):
-#         return hypot(self.x - other.x, self.y - other.y) # исп. готовую ф-ю из библ.math
-
-# Расчёт расстояния между A(5, 4), B(10, 2) точками на координатной плоскости
-# p1 = Point(5, 4) # коордиинаты первой точки на коорд.плоскости
-# p2 = Point(10, 2) # коордиинаты второй точки на коорд.плоскости
-# print(p1-p2) # вывод значений массива длин катетов
-# print(p1+p2) # вывод расчёта гипотенузы
-#             # (расстояния между 2-мя точками на координат.плоскости)
-# print(29**0.5) # проверка метода расчёта расстояния между 2-мя точками на координат.плоскости
-# p = [Point(), Point()]
-# p = Point()
-# print(p)
-#str(a) -> a.__str__ (одинаковый перевод в строку)
-
-
-# isinstance (объект, тип) -> True
-# isinstance (объект, (тип 1, тип 2, ... , тип N) -> True (если список, то действуем по другому сценарию)
-
-# lst = list(range(1, 15))
-# lst += ['a'] # добавим элемент списка не явл. целым числом
-#
-# class Stat:
-#         def __init__(self, vals):# класс возвращает мин, макс и среднее арифметическое
-#             self.values = vals[:]
-#
-#         def is_int(self) -> bool:
-#             return all(isinstance(item, int) for item in self.values)
-#
-#         def get_min(self):
-#             if self.is_int():
-#                 return min(self.values)
-#             return None
-#
-#         def get_max(self):
-#             if self.is_int():
-#                 return min(self.values)
-#             return None
-#
-#         def get_aver(self):
-#             if self.is_int():
-#                 return sum(self.values) / len(self.values)
-#             return None
-#
-# s =  Stat(lst)
-# print(s.get_min())
-# print(s.get_max())
-# print(s.get_aver())
-
-# class Selector():
-#     def __init__(self, vals):
-#         self.values = vals[:] # делаем копию списочного выражения, чтобы не изм. начальный список
-#
-#     def get_odd(self):
-#         return [x for x in self.values if x % 2] # остаток от деления не пустое значение (можно ==1)
-#
-#     def get_even(self):
-#         return [x for x in self.values if x % 2 ==0]
-#
-# s =  Selector(lst)
-# print(s.get_odd())
-# print(s.get_even())
-# print(lst)
-
-# from lib import Student, Employee, Person
-#
-# people = [
-#     Person('Александр', 27),
-#     Student('Дмитрий', "ГУАП"),
-#     Employee('Владимир', 'Авангард')
-# ]
-#
-# for person in people:
-#     if isinstance(person, Student):
-#         print(person.get_univ())
-#     elif isinstance(person, Employee):
-#         print(person.get_comp())
-#     else:
-#         print(person.get_name(), person.get_age())
-
-#from lib import Rectangle, Circle, Square
-
-# from math import pi
-#
-# class Circle:
-#     def __init__(self, radius):
-#         self.radius = radius
-#         self.name = 'круг'
-#
-#     def perimetr(self):
-#         return 2 * pi * self.radius
-#
-#     def area(self):
-#         return pi * self.radius ** 2
-#
-#     def get_name(self):
-#         return self.name
-#
-#
-# class Square:
-#     def __init__(self, side):
-#         self.side = side
-#         self.name = 'квадрат'
-#
-#     def perimetr(self):
-#         return 4 * self.side
-#
-#     def area(self):
-#         return self.side ** 2
-#
-#     def get_name(self):
-#         return self.name
-#
-# class Rectangle:
-#     def __init__(self, width, height):
-#         self.width = width
-#         self.height = height
-#         self.name = 'прямоугольник'
-#
-#     def perimetr(self):
-#         return 2 * (self.width + self.height)
-#
-#     def area(self):
-#         return self.width * self.height
-#
-#     def get_name(self):
-#         return self.name
-# # # 1-й способ
-# # # def shape_info(shape: object):
-# # #     print(f'Площадь {shape.get_name()}a: {shape.area()}, Периметр: {shape.perimetr()}')
-# # #     # функция shape_info различает обращение к вычисляемым значениям по классу объекта
-# #
-# # 2-й способ через isinstance
-# rect, circ, sqr = ['прямоугольник', 'круг', 'квадрат']
-# fig = ''
-# def shape_info(shape: object):
-#     if isinstance(shape, Circle):
-#         fig = circ
-#     elif isinstance(shape, Rectangle):
-#         fig = rect
-#     else:
-#         fig = sqr
-#
-#     print(f'Площадь {fig}a: {shape.area()}, Периметр: {shape.perimetr()}')
-
-# s = Square(10)
-# shape_info(s)
-#
-# cr = Circle(10)
-# shape_info(cr)
-#
-# r = Rectangle(5, 2)
-# shape_info(r)
-#
-# print(dir(s))
-# print(dir(cr))
-
-# class Book:
-#     def __init__(self, title, author):
-#         self._title = title
-#         self._author = author
-#
-#     def get_title(self):
-#         return self._title
-#
-#     def get_author(self):
-#         return self._author
-#
-# book = Book('Язык С++', 'Бьярн Страупструп')
-#
-# print(f'{book.get_title(), book.get_title()}')
-
-
-# print(1 + 2)
-# print(1 + 2.0)
-# print('abc' + 'def')
-# print([1, 2] + [3, 4])
-#
-# def func(x, y):
-#     return x + y
-#
-# print(func(2, 3.0))
-
-# ООП (polymorphism - свойство кода работать с разными типами данных)
-# method override; operator overloadind
-# (термины: переопределения метода и переопределения оператора)
-# "утиная типизация" - функция-оператор сама различает к какому классу относится операнд
