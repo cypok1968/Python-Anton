d6a731e3638448054bbd72c724cf1d8856ac1fbf
Anton <cypku@mail.ru>
2025-07-15 11:47:19 +0300

    SQL via Python (READ)

--- first project.py
+++ first project.py
@@ -1,104 +1,34 @@
-# JSON - (Java Script Object Notation) - не родной формат Питона, а формат Java Script (преобразуется в Питон)
-# load () - метод для чтения из файла JSON
-# loads () - метод для чтения строкового представления
-# зарегистрироваться на сайте openweathermap.org, взять после регистрации ключ для обращения к серверу !!!
-# import json
-#
-# d = {
-#     'ананас': 300,
-#     'банан': 400,
-#     'яблоко': 120,
-#     'груша': 280,
-# }
-# # # запись напрямую в файл
-# with open('fruits.json', 'w', encoding='utf-8') as f:
-#     json.dump(d, f, indent=4)
-# print()
-# вывод в виде строки
-# print(json.dumps(d, indent=4))
-# print(data)
+# База данных (чтение)
+"""
+1. Импорт библиотеки SQL
+2. Подключаемся к БД
+3. Назначить "курсор"
+4. Работаем с БД (запросы и ответы)
+5. Отключаемся от БД
+"""
+import sqlite3
 
+# Подключаемся
+connection = sqlite3.connect('db/movies.sqlite')
 
+# Курсор
+cursor = connection.cursor()
 
-# with open('dogs.json', 'rt') as d:
-#     # data = json.load(d) # для последующего чтения напрямую из файла
-# # print(data) # простой вывод инфо из словаря
-#     temp = d.read() # читаем файл как строку
-#     data = json.loads(temp) # строковое представление JSON
-#
-# for i in range(len(data)):
-#     print(f'Питомец: {i+1}')
-#     for k, v in data[i].items():
-#         if type(v) == list:
-#             print(f'{k}: {', '.join(v)}')
-#         else:
-#             print(f'\t{k}: {v}')
+# Запрос (с помощью курсора)
+result = cursor.execute(
+    """
+    SELECT title, year FROM films
+    WHERE year BETWEEN 2001 AND 2005
+    """
+)
 
-# for k, v in data.items():
-#     if type(v) == list:
-#         print(f'{k}: {', '.join(v)}')
-#     else:
-#         print(f'{k}: {v}')
+# print(result)
 
+# fetchall - всё
+# fetchone - только первое соответствие
+# fetchmany(N) - N - соответствий
 
-# ZIP
-from zipfile import is_zipfile, ZipFile
-import os
+array = result.fetchall()
 
-# csv_files = [f for f in os.listdir() if f.endswith('csv')] # вывод списка файлов
-# # print(csv_files)
-# with ZipFile('archive.zip', 'w') as myzip: # упаковка в архив с последующим удалением самих файлов
-#     for file in csv_files:
-#         myzip.write(file)
-#         os.remove(file)
-
-# files_to_extract = ['people.csv', 'files.csv']
-#
-# # Распаковать
-# with ZipFile('archive.zip', 'w') as zip_obj: # распаковка из архива
-#     #zip_obj.extractall() # распаковка всех файлов
-#     zip_obj.extractall(members=files_to_extract) # выборочная распаковка
-
-
-# CSV-файлы (strptime)
-# import  csv
-# from os import write
-
-# data = [
-#     ['name', ' age', ' city'],
-#     ['Андрей', ' 27', ' Казань'],
-#     ['Игорь', ' 31', ' Москва'],
-#     ['Сергей', ' 25', ' Тверь']
-# ]
-#
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     reader = csv.reader(f, delimiter=',', quotechar='"')
-#     for row in reader:
-#         print(row)
-#
-# with open('employee.csv', 'w', encoding='utf-8') as f:
-#     write = csv.writer(f)
-#     write.writerow(data)
-#
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     dict_reader = csv.DictReader(f)
-#     for row in dict_reader:
-#         print(f'{row['name']} живёт в городе {row['city']}')
-
-# field_names = ['name', ' age', ' city']
-# data = {
-#     'name', 'Андрей',
-#     'age', 27,
-#     'city', 'Москва',
-# }
-#
-# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.DictWriter(f, fieldnames=field_names)
-#     writer.writerow(data)
-
-# Режимы квотирования
-# data = ['name', 25, 'town']
-# with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-#     writer.writerow(data)
-
+for title, year in array:
+    print(title, year)
---
+++ sql_samples.md
@@ -0,0 +1,94 @@
+
+
+
+# 
+"""
+SELECT title, year
+FROM films
+WHERE year > 2005
+AND year < 2010
+AND duration < 90
+ORDER by year
+"""
+"""
+SELECT title, year
+FROM films
+WHERE year BETWEEN 2005 AND 2010
+AND duration < 90
+ORDER by year
+"""
+# Пример не вполне корректного запроса
+"""
+SELECT title
+"""
+# Исправим составной запрос
+"""
+SELECT title FROM films
+WHERE genre = (
+SELECT id FROM genres
+WHERE title = 'фантастика')
+"""
+# Выборка по пречню значений
+"""
+SELECT title, duration FROM films
+WHERE duration IN (45, 60, 90)
+ORDER BY duration DESC
+"""
+# по убывваанию длительности фильма
+
+# Выборка с группировкой по ID (по возрастанию)
+"""
+SELECT * FROM films
+WHERE year >= 2001
+AND duration BETWEEN 45 and 90
+GROUP BY id
+"""
+
+# Название фильма на А и содержит любое число букв "к"
+### Примечание LIKE:
+- % - любое количество символов от 0 до INF
+- _ - любой символ
+- можно и NOT LIKE
+"""
+SELECT title FROM films
+WHERE title LIKE 'А_к%'
+"""
+"""
+SELECT title FROM films
+WHERE title NOT LIKE 'А_к%'
+"""
+
+# Выборка без повторов
+### Примечание:
+- DISTICT
+"""
+SELECT DISTICT year from films
+ORDER by year
+"""
+# Выборка с объединением 2-х таблиц
+# Составляем сводную таблицу с названиями столбцов
+# на русском языке (выбираем фильмы и жанры в столбцы)
+"""
+SELECT
+films.title as Фильм,
+genres.title as Жанр
+FROM films
+JOIN genres
+WHERE films.genre = genres.id
+"""
+# Выборка: сколько фильмов каких годов 
+"""
+SELECT year, count(*) as Кол_во
+FROM films
+GROUP BY year
+ORDER BY Кол_во DESC
+"""
+# Выборка: сколько фильмов каких годов,
+# количество которых более 500
+"""
+SELECT year, count(*) as Кол_во
+FROM films
+
+GROUP BY year
+ORDER BY Кол_во DESC
+"""
