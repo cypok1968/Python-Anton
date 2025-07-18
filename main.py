b80a74ef1a9a102c67f83985e29b882a0b7cdf4a
Anton <cypku@mail.ru>
2025-07-18 12:11:51 +0300

    Query Updater 18/07/25

--- main.py
+++ main.py
@@ -218,20 +218,19 @@ if __name__ == '__main__':
     db_session.global_init('db/news.sqlite')
     # app.run(host='127.0.0.1', port=5000, debug=debug)
     user = User()
-    user.name = 'User2'
-    user.about = 'Данные про User2'
-    user.email = 'b@c.ru'
+    # user = User()
     db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.commit()
     first = db_sess.query(User).filter((User.id != 1) | (User.email.not_like('%a%'))).all()
     print(first)
+    user = db_sess.query(User).filter(User.id == 2).first()
+    user.name = 'Billy'
+    db_sess.commit()
+    print(user)
     # user.name = 'User2'
     # user.about = 'Данные про User2'
     # user.email = 'b@c.ru'
     # db_sess = db_session.create_session()
     # db_sess.add(user)
-    # db_sess.commit()
 
 
 # МОЯ СТРАНИЦА от 17/07/25 для чтения комментов (замещена)
