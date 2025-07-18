1c07b1bc2388baf06dfc4bce13b5d2748520f9ed
Anton <cypku@mail.ru>
2025-07-18 10:49:02 +0300

    News and User Tables 18/07/25

--- data/users.py
+++ data/users.py
@@ -1,25 +1,25 @@
 import datetime
 import sqlalchemy
-from sqlalchemy import Column
-
 from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
 
 
-class User(SqlAlchemyBase): # создаем новую таблицу, указание нового имени обязательно,
-                            # чтоб имя не User (user)
+class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True,
                            autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, # создаем колонки новой БД
+    name = sqlalchemy.Column(sqlalchemy.String,
                              nullable=True)
     about = sqlalchemy.Column(sqlalchemy.String,
-                             nullable=True)
+                              nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
-                             index=True,
-                             nullable=True)
+                              index=True, unique=True,
+                              nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String,
-                                       nullable=True)
+                                        nullable=True)
     create_data = sqlalchemy.Column(sqlalchemy.DateTime,
-                                       default=datetime.datetime)
+                                    default=datetime.datetime.now())
+
+    news = orm.relationship("News", back_populates='user')
