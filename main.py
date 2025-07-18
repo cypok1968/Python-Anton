76d1d1de64bfe71a271f776d3552e2f5a241eb2c
cypok1968 <cypku1968@gmail.com>
2025-07-18 13:06:50 +0300

    NewS Add 18/07/25

--- main.py
+++ main.py
@@ -9,7 +9,6 @@
 # ORM - Object Relational Mapping
 # DBeaver - универсальный (мощный) софт для работы с БД SQL (помимо SQLite, менее функциональна)
 import os.path
-
 import sqlite3
 from sqlite3 import Error
 from forms.loginform import LoginForm
