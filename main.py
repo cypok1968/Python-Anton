3904f8daf70e3e8d2e78204b1b22296c6a811372
Anton <cypku@mail.ru>
2025-07-21 10:52:16 +0300

    News Job 2 corr 21/07/25

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ from data.news import News
 from data.users import User
 from forms.loginform import LoginForm
 from forms.user import Register
-from flask_login import LoginManager, login_user, logout_user
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 app = Flask(__name__)
@@ -46,7 +45,7 @@ def load_user(user_id):
 
 
 @app.errorhandler(404)
-@@ -49,27 +49,32 @@
+def not_found(e):
     return render_template('404.html', title='Не найдено')
 
 
@@ -79,7 +78,19 @@ def contacts():
 
 
 @app.route('/login', methods=['GET', 'POST'])
-@@ -89,27 +94,28 @@
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html',
+                               message='Неверный логин или пароль',
+                               title='Ошибка авторизации',
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/logout')
@@ -108,7 +119,147 @@ def register():
                                    title='Регистрация',
                                    message='Такой пользователь уже есть',
                                    form=form)
-@@ -257,57 +263,61 @@
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html',
+                           title='Регистрация', form=form)
+
+
+@app.route('/countdown')
+def cd():
+    lst = [str(x) for x in reversed(range(10))]
+    lst.append('Полетели!!!')
+    return '<br>'.join(lst)
+
+
+@app.route('/image')
+def show_image():
+    return f'<img src="{url_for('static', filename='images/python.jpg')}">'
+
+
+@app.route('/sample-page')
+def sample_page():
+    return f"""<!DOCTYPE html>
+            <html lang="ru">
+            <head>
+                <meta charset="UTF-8">
+                <title>Картинка Питона</title>
+            </head>
+            <body>
+                <img src="{url_for('static', filename='images/python.jpg')}" alt="Python">
+            </body>
+            </html>
+    """
+
+
+@app.route('/sample-page2')
+def sample_page2():
+    with open('old/temp.html', 'r', encoding='utf-8') as html:
+        return html.read()
+
+
+# Так делать мы не будем
+# x = 5
+# @app.route('/1')
+# def show_num():
+#     global x
+#     x += 1
+#     return str(x)
+
+# <string> - по умолчанию строка
+# <int:number> - целое
+# <float:number> - дес. дробь
+# <path:p> - может содержать слэши для указания пути
+# <uuid:id> - строка-идентификатор (16-байт в HEX-формате)
+@app.route('/greeting/<string:user>/<int:id_num>')
+def greeting(user, id_num):
+    return f'Привет, {user} c id={id_num}'
+
+
+@app.route('/get-user/')
+@app.route('/get-user/<int:id_num>')
+def get_user(id_num=None):
+    try:
+        # Подключение к базе данных
+        con = sqlite3.connect('db/movies.sqlite')
+        cur = con.cursor()
+
+        if id_num is None:
+            # Получение списка всех пользователей
+            query = 'SELECT trip_id, name FROM users'
+            response = cur.execute(query)
+            result = response.fetchall()
+            return render_template('get_user.html', users=result)
+
+        # Получение информации о конкретном пользователе
+        query = 'SELECT name, city, date_first FROM users WHERE trip_id=?'
+        response = cur.execute(query, (id_num,))
+        result = response.fetchone()
+
+        if result:
+            name, city, date_first = result
+            return render_template('get_user.html',
+                                   name=name,
+                                   city=city,
+                                   start=date_first)
+        else:
+            return "Пользователь не найден", 404
+
+    except Error as e:
+        return f"Произошла ошибка: {str(e)}", 500
+
+    finally:
+        # Гарантированное закрытие соединения
+        if con:
+            cur.close()
+            con.close()
+
+
+@app.route('/form-test', methods=['POST', 'GET'])
+def form_test():
+    if request.method == 'GET':
+        with open('old/form.html', 'r', encoding='utf-8') as html:
+            return html.read()
+    elif request.method == 'POST':
+        print(request.form)
+        return 'Форма успешно отправлена'
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def file_upload():
+    if request.method == 'GET':
+        with open('old/upload.html', 'r', encoding='utf-8') as html:
+            return html.read()
+    elif request.method == 'POST':
+        # print(request.files)
+        if 'file' not in request.files:
+            return 'Файл не был выбран!!!'
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return 'Файл не был выбран!!!'
+
+        if file and allowed_file(file.filename):
+            new_name = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], new_name))
+            return f'Файл {new_name} успешно загружен!'
+    return "Ошибка загрузки"
+
+
+@app.route('/numbers/')
+@app.route('/numbers/<int:num>')
+def odd_even(num=None):
+    if num is None:
+        return render_template('numbers.html',
+                               title='Нет числа', number='')
     return render_template('numbers.html',
                            title='Чет-нечёт', number=num)
 
@@ -133,7 +284,6 @@ def queue():
 @app.route('/news')
 def news():
     db_sess = db_session.create_session()
-    all_news = db_sess.query(News).filter(News.is_private != True).all()
     if current_user.is_authenticated:
         all_news = db_sess.query(News).filter(
             (News.user == current_user) | (News.is_private != True)).all()
