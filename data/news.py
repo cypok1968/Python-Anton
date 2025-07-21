b327edad8fa29f518e56cc8a5e09346c65d0c587
Anton <cypku@mail.ru>
2025-07-21 14:03:36 +0300

    About only for Admin 21/07/25

--- data/news.py
+++ data/news.py
@@ -1,10 +1,23 @@
-from flask_wtf import FlaskForm
-from wtforms.fields.simple import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
 
 
-class NewsForm(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired('Введите заголовок')])
-    content = TextAreaField('Содержание')
-    is_private = BooleanField('Личное')
-    submit = SubmitField('Применить')
+class News(SqlAlchemyBase):
+    __tablename__ = 'news'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True,
+                           autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String,
+                             nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=True)
+    create_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                    default=datetime.datetime.now())
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean,
+                              default=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                        sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
