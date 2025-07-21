936e83662c143691334fd5c62e50832f7437126d
Anton <cypku@mail.ru>
2025-07-21 12:13:23 +0300

    Cookies and Session 21/07/25

--- sesstest.py
+++ sesstest.py
@@ -1,27 +1,38 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'just_secret_key'
 
 
 @app.route('/')
 def index():
-    return redirect('cookie_test')
-
+    return redirect('/cookie_test')
 
 
 @app.route('/cookie_test')
 def cookie_test():
-    visit_count = int(request.cookies.get('visit_count', 0)) # если ключ кукисов был найден, то переменная возвращает ноль
+    visit_count = int(request.cookies.get('visit_count', 0))
 
     if visit_count:
         res = make_response(f'Вы посетили эту страницу {visit_count} раз(а).')
-        res.set.cookies('visit_count', str(visit_count + 1),
-                        max_age=60 * 60 * 24 * 30) # определяем сколько времени будут жить кукисы
+        res.set_cookie('visit_count', str(visit_count + 1),
+                       max_age=60 * 60 * 24 * 30)
     else:
-        res = make_response('Вы впервые тут у нас за этот месяц')
+        res = make_response('Вы впервые тут у нас за этот месяц!')
         res.set_cookie('visit_count', '1',
-                        max_age=60 * 60 * 24 * 30)
-        return res
+                       max_age=60 * 60 * 24 * 30)
+    return res
+
+
+@app.route('/session_test')
+def session_test():
+    visit_count = session.get('visit_count', 0)
+    print(visit_count)
+    session['visit_count'] = visit_count + 1
+    return make_response(
+        f'Вы на этой странице {visit_count + 1} раз(а)'
+    )
+
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=8000)
